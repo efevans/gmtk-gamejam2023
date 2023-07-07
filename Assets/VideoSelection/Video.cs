315a98866ddcdcafbd2e497dddb2b7ad05ea52b5
efevans <efevans1@gmail.com>
2023-07-07 16:27:24 -0700

    Change row of columns to grid layout

--- Assets/VideoSelection/Video.cs
+++ Assets/VideoSelection/Video.cs
@@ -12,16 +12,4 @@ public class Video : MonoBehaviour, IPointerClickHandler
             Debug.Log("Clicked video!");
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
